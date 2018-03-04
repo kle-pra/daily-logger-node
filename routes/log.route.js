cf93ccbaa2295b266cad52cf5050dc5958419371
klepra <klemen.pravdic@gmail.com>
2018-03-04 10:55:29 +0100

    archive component fixes

--- routes/log.route.js
+++ routes/log.route.js
@@ -8,14 +8,11 @@ router.get('/:id', function (req, res) {
             console.log(err);
             return res.json({ error: error });
         }
-
         return res.json(data);
-
     });
 })
 
 router.get('/', function (req, res) {
-
     //find all logs for the day from date param else all
     if (req.query.date) {
         let date = new Date(req.query.date);
@@ -23,8 +20,6 @@ router.get('/', function (req, res) {
         let day = date.getDate() + 1;
         let year = date.getFullYear();
         let dateString = year + "-" + month + "-" + day;
-        console.log(dateString);
-        console.log(new Date(year, month, day));
         Log.find(
             // { '$where': 'this.date.toJSON().slice(0, 10) == "' + dateString + '"' }
             { date: { $gte: new Date(year, month, day - 1), $lt: new Date(year, month, day) } }
@@ -37,16 +32,13 @@ router.get('/', function (req, res) {
                 return res.json(data);
 
             });
-
     } else {
         Log.find({}, function (err, data) {
             if (err) {
                 console.log(err);
                 return res.json({ error: error });
             }
-
             return res.json(data);
-
         });
     }
 })
